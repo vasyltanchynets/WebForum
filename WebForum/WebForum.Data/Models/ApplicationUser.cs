f4e5526520186cc04bdf3cbe32ec3cb8195fd3fd
Vasyl <vasyltanchinets98@gmail.com>
2021-02-21 01:22:19 +0200

    updated-ApplicationUser

--- WebForum/WebForum.Data/Models/ApplicationUser.cs
+++ WebForum/WebForum.Data/Models/ApplicationUser.cs
@@ -5,6 +5,9 @@ namespace WebForum.Data.Models
 {
     public class ApplicationUser : IdentityUser
     {
-
+        public int Rating { get; set; }
+        public string ProfileImageUrl { get; set; }
+        public DateTime MemberSince { get; set; }
+        public bool IsActive { get; set; }
     }
 }
