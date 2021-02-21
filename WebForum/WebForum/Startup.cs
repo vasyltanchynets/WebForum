d74d9a174b2bfdd50b98a4ee345e7c128346eab0
Vasyl Tanchynets <vasyltanchinets98@gmail.com>
2021-02-22 00:31:36 +0200

    fix

--- WebForum/WebForum/Startup.cs
+++ WebForum/WebForum/Startup.cs
@@ -31,7 +31,7 @@ namespace WebForum
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddScoped<IForum, ForumService>();
