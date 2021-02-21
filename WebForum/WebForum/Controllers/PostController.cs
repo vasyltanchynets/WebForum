df7188e207681c1d3bf2d0dfb93aa36ed3f75ef3
Vasyl Tanchynets <vasyltanchinets98@gmail.com>
2021-02-21 18:46:36 +0200

    was-created-controller-Post

---
+++ WebForum/WebForum/Controllers/PostController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using WebForum.Data;
+using WebForum.Data.Models;
+using WebForum.Models.Post;
+using WebForum.Models.Reply;
+
+namespace WebForum.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly IPost _postService;
+
+        public PostController(IPost postService)
+        {
+            _postService = postService;
+        }
+
+        public IActionResult Index(int id)
+        {
+            var post = _postService.GetById(id);
+            var replies = BuildPostReplies(post.Replies);
+
+            var model = new PostIndexModel
+            {
+                Id = post.Id,
+                Title = post.Title,
+                AuthorId = post.User.Id,
+                AuthorName = post.User.UserName,
+                AuthorImageUrl = post.User.ProfileImageUrl,
+                AuthorRating = post.User.Rating,
+                Created = post.Created,
+                PostContent = post.Content,
+                Replies = replies
+            };
+
+            return View(model);
+        }
+
+        private IEnumerable<PostReplyModel> BuildPostReplies(IEnumerable<PostReply> replies)
+        {
+            return replies.Select(reply => new PostReplyModel
+            {
+                Id = reply.Id,
+                AuthorName = reply.User.UserName,
+                AuthorId = reply.User.Id,
+                AuthorImageUrl = reply.User.ProfileImageUrl,
+                AuthorRating = reply.User.Rating,
+                Created = reply.Created,
+                ReplyContent = reply.Content
+            });
+        }
+    }
+}
