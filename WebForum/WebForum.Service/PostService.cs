11d38f473009cac4760556f3fedae6fef309486e
Vasyl Tanchynets <vasyltanchinets98@gmail.com>
2021-02-21 21:06:14 +0200

    added-post-service-method-GetById

--- WebForum/WebForum.Service/PostService.cs
+++ WebForum/WebForum.Service/PostService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -38,7 +39,13 @@ namespace WebForum.Service
 
         public Post GetById(int id)
         {
-            throw new NotImplementedException();
+            return _context.Posts
+                .Where(post => post.Id == id)
+                .Include(post => post.User)
+                .Include(post => post.Replies)
+                    .ThenInclude(reply => reply.User)
+                .Include(post => post.Forum)
+                .First();
         }
 
         public IEnumerable<Post> GetFilteredPosts(string searchQuery)
