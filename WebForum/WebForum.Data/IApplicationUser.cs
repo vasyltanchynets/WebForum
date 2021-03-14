7a717fdacb01eebaf7a5eb8792fd0b9785378bba
Vasyl Tanchynets <vasyltanchinets98@gmail.com>
2021-03-14 14:34:14 +0200

    was-created-services-and-interfaces-for-user-profile

---
+++ WebForum/WebForum.Data/IApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebForum.Data.Models;
+
+namespace WebForum.Data
+{
+    public interface IApplicationUser
+    {
+        ApplicationUser GetById(string id);
+        IEnumerable<ApplicationUser> GetAll();
+       
+        Task SetProfileImage(string id, Uri uri);
+        Task IncrementRating(string id, Type type);
+    }
+}
